bc4b27c56e5a1b1bffa603ff870d1b7ba2534979
Mattia Orlandi <mattia.orlandi21@gmail.com>
2021-05-13 00:22:22 +0200

    Added benchmark for dual model

---
+++ CP/benchmark/dual.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cores=$1
+if [[ cores -eq "" ]]
+then
+    cores=1
+fi
+echo "Usinf $cores cores"
+for n in 8 9 10 11 12; do
+    echo -e "\tTest naive model with N=$n"
+    minizinc --solver Gecode -a -p $cores --solver-statistics src/pwp-naive.mzn "in/${n}x${n}.dzn" | grep -E 'solutions|failures'
+    echo -e "\tTest dual model with N=$n"
+    minizinc --solver Gecode -a -p $cores --solver-statistics src/pwp-dual.mzn "in/${n}x${n}.dzn" -D mzn_ignore_symmetry_breaking_constraints=true | grep -E 'solutions|failures'
+    echo -e "\tTest dual model + symmetry breaking with N=$n"
+    minizinc --solver Gecode -a -p $cores --solver-statistics src/pwp-dual.mzn "in/${n}x${n}.dzn" | grep -E 'solutions|failures'
+done

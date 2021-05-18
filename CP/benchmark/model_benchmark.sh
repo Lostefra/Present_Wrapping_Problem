e41ee5c7a386a71e2b0549fd47c9d5686985617d
Mattia Orlandi <mattia.orlandi21@gmail.com>
2021-05-19 00:16:20 +0200

    Adjusted dual and final models

--- CP/benchmark/model_benchmark.sh
+++ CP/benchmark/model_benchmark.sh
@@ -6,17 +6,21 @@ if [[ cores -eq "" ]]; then
 fi
 echo "Using $cores cores"
 
-for n in 10 11 12 13 14 15 16; do
+for n in $(seq 10 15); do
     echo -e "\tTest naive model with N=$n"
     for _ in $(seq 5); do
-	    minizinc --solver Gecode -p $cores --solver-statistics src/pwp-naive.mzn "in/${n}x${n}.dzn" | grep -E 'solveTime|failures'
+	    minizinc --solver Gecode -p $cores -a -t 60000 --solver-statistics src/pwp-naive.mzn "in/${n}x${n}.dzn" | grep -E 'solveTime|solutions|failures'
     done
     echo -e "\tTest dual model with N=$n"
     for _ in $(seq 5); do
-	    minizinc --solver Gecode -p $cores --solver-statistics src/pwp-dual.mzn "in/${n}x${n}.dzn" | grep -E 'solveTime|failures'
+	    minizinc --solver Gecode -p $cores -a -t 60000 --solver-statistics src/pwp-dual.mzn "in/${n}x${n}.dzn" | grep -E 'solveTime|solutions|failures'
     done
-    echo -e "\tTest final model with N=$n"
+    echo -e "\tTest final model (no symmetry breaking) with N=$n"
     for _ in $(seq 5); do
-	    minizinc --solver Gecode -p $cores --solver-statistics src/pwp-final.mzn "in/${n}x${n}.dzn" | grep -E 'solveTime|failures'
+	    minizinc --solver Gecode -p $cores -a -t 60000 --solver-statistics src/pwp-final.mzn "in/${n}x${n}.dzn" -D mzn_ignore_symmetry_breaking_constraints=true | grep -E 'solveTime|solutions|failures'
+    done
+    echo -e "\tTest final model (symmetry breaking) with N=$n"
+    for _ in $(seq 5); do
+	    minizinc --solver Gecode -p $cores -a -t 60000 --solver-statistics src/pwp-final.mzn "in/${n}x${n}.dzn" | grep -E 'solveTime|solutions|failures'
     done
 done
